3fead148ab4c44949d1424a7a08a345b8e2bb398
Joel Richards <joelrichards80@gmail.com>
2020-07-29 14:35:25 +0100

    first commit

---
+++ src/pages/account.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Router } from "@reach/router"
+import { login, logout, isAuthenticated, getProfile } from "./utils/auth"
+import { Link } from "gatsby"
+
+const Home = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
+const Settings = () => <p>Settings</p>
+const Quiz = () => <p>Quiz</p>
+
+const Account = () => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
+
+  const user = getProfile()
+
+  return (
+    <>
+      <nav>
+        <Link to="/account/">Home</Link>{" "}
+        <Link to="/account/settings/">Settings</Link>{" "}
+        <Link to="/account/quiz/">Quiz</Link>{" "}
+        <a
+          href="#logout"
+          onClick={e => {
+            logout()
+            e.preventDefault()
+          }}
+        >
+          Log Out
+        </a>
+      </nav>
+      <Router>
+        <Home path="/account/" user={user} />
+        <Settings path="/account/settings" />
+        <Quiz path="/account/quiz" />
+      </Router>
+    </>
+  )
+}
+
+export default Account
